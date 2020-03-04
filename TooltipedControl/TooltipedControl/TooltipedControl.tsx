1851d14979de688512913374ab6ae57e6a532d3e
eric liu <szliuxf@gmail.com>
2020-03-03 23:36:27 -0800

    Text control with tool tip initial check in.

---
+++ TooltipedControl/TooltipedControl/TooltipedControl.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+import {IInputs} from "./generated/ManifestTypes";
+import { TextField} from 'office-ui-fabric-react/lib/TextField';
+import { TooltipHost, ITooltipHostStyles } from 'office-ui-fabric-react/lib/Tooltip';
+import { concatStyleSetsWithProps } from 'office-ui-fabric-react/lib/Styling';
+
+export interface IProps {
+    value:  string | null ,
+    tooltip: string | null ,
+    valueChanged: (newValue: string) => void; 
+}
+
+export const TooltipedControlCompoment : React.FC<IProps> = (props) => {        
+
+    // react hook to store the field value.
+    const [value, setValue] = React.useState("");
+
+    const onChangeValue = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) =>   {
+        setValue(newValue == undefined ? "" : newValue );
+
+        if (props.valueChanged != null) {
+            props.valueChanged(newValue == undefined ? "" : newValue) ;
+        }
+
+    }
+    
+    return (
+            <TooltipHost content = { props.tooltip != null ? props.tooltip : "" } >
+                <TextField value = { props.value != null ? props.value : undefined } onChange={onChangeValue} />
+            </TooltipHost>
+    );
+};
